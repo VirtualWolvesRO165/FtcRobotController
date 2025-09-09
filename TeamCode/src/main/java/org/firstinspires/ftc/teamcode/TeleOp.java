bd7690c5cf7706b01114b73f130c83cf855c2090
VirtualWolvesRO165 <virtualwolvesro165@gmail.com>
2025-09-09 21:04:54 +0300

    added TeleOp structure

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.java
@@ -0,0 +1,21 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+
+@com.qualcomm.robotcore.eventloop.opmode.TeleOp
+public class TeleOp extends LinearOpMode {
+    @Override
+    public void runOpMode() {
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+        waitForStart();
+
+        while (opModeIsActive()) {
+            telemetry.update();
+        }
+    }
+}
+
