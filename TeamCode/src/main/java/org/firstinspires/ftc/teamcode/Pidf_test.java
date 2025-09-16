2cea317b8f52ce6b7531375c668e0e8599eeb262
Pavala1243 <andreibd2009@yahoo.com>
2025-09-16 19:29:50 +0300

    PID

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Pidf_test.java
@@ -0,0 +1,21 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.arcrobotics.ftclib.controller.PIDController;
+import com.pedropathing.control.PIDFController;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+@TeleOp
+public class Pidf_test extends OpMode{
+        private PIDFController controller;
+
+@Override
+    public init(){
+
+}
+@Override
+    public void loop(){
+
+    }
+}
